30fe7ee42d5e0172fa30d7746fd61a9db564dab9
DGBIZT <georgiy.gorlov@gmail.com>
2025-06-23 23:13:21 +0300

    лучшие практики API

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import os
-a = 5
+from dotenv import load_dotenv
+import requests
+
+
 
 if __name__ == '__main__':
-    print(a)
+    load_dotenv()
+    api_key = os.getenv('API_KEY')
+    print(api_key)
+
+    username = "primitivetechnology9550"
+    url = f"https://www.googleapis.com/youtube/v3/channels?part=id&forUsername={username}&key={api_key}"
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+
+    # channel_ids = [
+    #     'UC-OVMPlMA3-YCIeg4z5z23A', # moscowpython
+    #     'UCwHL6WHUarjGfUM_586me8w', # highload
+    # ]
+
+    # data = get_youtube_data(api_key, channel_ids)
